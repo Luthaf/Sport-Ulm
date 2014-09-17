3b55504096c4fceb77e3653482a0d4a1e7bbb92f
Luthaf <luthaf@luthaf.fr>
2014-09-17 14:19:49 +0200

    Change URL to fit ReST conventions

--- sport@ulm/urls.py
+++ sport@ulm/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('bds.urls')),
-    url(r'^user/', include('profilENS.urls')),
+    url(r'^users/', include('profilENS.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
