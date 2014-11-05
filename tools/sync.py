26277419383bb8adf4fd009ba26735bb88d421b2
cphyc <admin@cphyc.me>
2014-11-05 15:02:49 +0100

    Import function is working
    TODO: have a better algorithm to find existing person and ask if you
    want to merge.

---
+++ tools/sync.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+import sys
+print (os.getcwd())
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sport@ulm.settings")
+    try:
+        from profilENS.models import User
+    except:
+        # return to the root of Sport-Ulm
+        os.chdir("..")
+        from profilENS.models import User
+
+
+    
+    current = {}
+    print ("[ FETCHING ]")
+    for user in User.objects.all():
+        current[user.username] = user
+    print ("[ SYNCING ]")
+    for line in sys.stdin:
+        bits = line.split(":")
+        username = bits[0]
+        full_name = bits[4].split(" ")
+        first_name = full_name[0]
+        last_name = " ".join(full_name[1:])
+        if username in current.keys():
+            user = current[username]
+            # Update first/last name if necessary
+            if user.first_name != first_name:
+                user.first_name = first_name
+                user.save()
+                print ("Updated first name of ", username)
+            if user.last_name != last_name:
+                user.last_name = last_name
+                user.save()
+                print ("Updated last name of ", username)
+        else:
+            user = User(username = username, first_name = first_name,
+                        last_name = last_name, email = username+"@clipper.ens.fr")
+            user.save()
+    print ("[ DONE ]")
