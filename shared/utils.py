847c31cdc9837b60ddac725419d84d1bef3e9516
Corentin Cadiou <contact@cphyc.me>
2015-07-26 14:53:10 +0200

    Fixed bug by converting all to list

--- shared/utils.py
+++ shared/utils.py
@@ -2,8 +2,8 @@ def get_model_fields(model):
     ''' Returns all the fields of a model'''
     fields = {}
     options = model._meta
-    for field in sorted(options.concrete_fields + \
-                        options.many_to_many + \
-                        options.virtual_fields):
+    for field in sorted(list(options.concrete_fields) + \
+                        list(options.many_to_many) + \
+                        list(options.virtual_fields)):
         fields[field.name] = field
     return fields
